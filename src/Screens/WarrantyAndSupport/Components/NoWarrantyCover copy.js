ff9515ebdf8bf9cde43b5b1bae9f154a437ce01e
Tehseen Jawed <51497155+TehseenJawed@users.noreply.github.com>
2023-12-23 05:07:39 +0500

    worked on contact us and warranty bug fixes.

--- src/Screens/WarrantyAndSupport/Components/NoWarrantyCover copy.js
+++ src/Screens/WarrantyAndSupport/Components/NoWarrantyCover copy.js
@@ -5,9 +5,9 @@ function NoWarrantyCover() {
   return (
     <div className="reorganize_container">
       <div className="ourmission-container">
-        <div className="ourmission-heading">WHAT THIS WARRANTY WILL NOT COVER:</div>
+        <div className="ourmission-heading ">WHAT THIS WARRANTY WILL NOT COVER:</div>
         <hr style={{ margin: "20px 0px" }} />
-        <div className="ourmission-subcontainer">
+        <div className="ourmission-subcontainer warranty-notcover">
           <div className="nowarranty-cover">
             <div>
             Any defects or damages due to improper use/care/maintenance/fabrication/installation/modifications/coverings/ treatments/scratches/normal tear and wear/temporary markings/staining/discoloration/fading/any variances in color, shade, size, shape/distribution of the pattern of the natural quartz or the natural variations in background tone/cracks due to force-other-impacts/abnormal use and conditions/commercial use-applications/acts of God/unpaid materials/vandalism/relocation of material/material that was inspected upon delivery to the fabricator-installer and accepted as defect-free upon receive/ rental use/ bending, curving, or other creative use of the Stone during or after installation/modifications made to fit change the load-other capacity of the material that could result in structural failure/material that has been milled or reduced in thickness/installed in any outdoor application where it’s exposed to elements/mishandling, irregular use/misuse or physical or chemical abuse or exposure/direct or sustained heat or cold or sudden or rapid change in temperature or thermal shock/abuse, excessive force or abrasive or corrosive substances/showers, steam-saunas, tiling, flooring, cladding applications/job site conditions/architectural and engineering designs/accidents/vertical surfaces application/non-standard applications other than in residential kitchen and baths.
