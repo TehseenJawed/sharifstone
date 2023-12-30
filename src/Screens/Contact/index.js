5394917c33eaca5abfd5418bcaab48ae95a732d3
Tehseen Jawed <51497155+TehseenJawed@users.noreply.github.com>
2023-12-30 14:49:13 +0500

    removed image tag

--- src/Screens/Contact/index.js
+++ src/Screens/Contact/index.js
@@ -12,8 +12,6 @@ function ContactScreen() {
         image={"url(images/cover/contact_us.png)"}
         label={["SPEAK", "WITH US"]}
       />
-      
-      <img src="https://lh3.googleusercontent.com/drive-viewer/AEYmBYSivPKFqfZiv1mJxs6x20BOkYSelu8uAGmWlie91T7-SoSoRTwqI-hxMLvwg-16X7pne6ccLlHiVlR6DEM9SGYL1TvOEg=w3300-h1964" alt="drive image"/>
       <div className="contact-maincontainer">
         <div className="contact-maindesc">
           Feel free to contact us with questions via our form below and we will
